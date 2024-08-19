21ca8771418411a501eddb9970370e726e4b0ada
Yogabayu <yogabayusbi@gmail.com>
2024-08-19 16:25:27 +0700

    uuu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': createProxyMiddleware({
+        target: 'https://www.virustotal.com/api/v3',
+        changeOrigin: true 
+      })
+    }
+  }
 })
